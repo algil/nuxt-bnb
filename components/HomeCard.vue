dcae628bcc4f1c3138c980781b46400301518b1e
Antonio Gil <antonioluisgil@gmail.com>
2021-02-16 20:36:56 +0100

    Add first component and list of houses

---
+++ components/HomeCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img :src="home.images[0]" :alt="home.title" style="width: 200px" />
+    <p>{{ home.title }}</p>
+    <p>
+      {{ home.location.address }} {{ home.location.city }}
+      {{ home.location.state }}
+    </p>
+    <p>${{ home.guests }} guests</p>
+    <p>{{ home.pricePerNight }} / night</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    home: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
