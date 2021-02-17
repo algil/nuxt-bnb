2c46862d65088bcc04b7dadb58e45e8a76aafb90
Antonio Gil <antonioluisgil@gmail.com>
2021-02-17 21:30:18 +0100

    Add the first route for home detail page

---
+++ pages/home/_id.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <template v-if="home">
+      <h1>{{ home.title }}</h1>
+    </template>
+    <p v-else>No home found</p>
+  </div>
+</template>
+
+<script>
+import homes from '@/data/homes'
+
+export default {
+  data() {
+    return {
+      home: {},
+    }
+  },
+
+  created() {
+    this.home = homes.find((home) => home.objectID === this.$route.params.id)
+  },
+}
+</script>
